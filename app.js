bdb14ed0a4776688459af6725171b11e6de21918
Jackson Foster <jacksonf1999@gmail.com>
2020-06-07 11:55:52 -0500

    Finished the JS tip calculator.

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ function totalCalc (a, b) {
     return a + b;
 }
 
-var mealCost = input(parseFloat("What was the cost of your meal?"))
-var tipRate = 0.2
-var tip = tipCalc(mealCost, tipRate)
-var totalCost = totalCalc(tip, mealCost)
+var mealCost = parseFloat(prompt("What was the cost of your meal?"));
+var tipRate = 0.2;
+var tip = tipCalc(mealCost, tipRate);
+var totalCost = totalCalc(tip, mealCost);
+var roundedTip = tip.toFixed(2);
+var roundedCost = totalCost.toFixed(2);
+
+
+
+document.write("Tip: $",roundedTip, ", Total Cost: $", roundedCost);
