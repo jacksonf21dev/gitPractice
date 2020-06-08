85d763752ab55db2ea6178520dc91be065192fba
Jackson Foster <jacksonf1999@gmail.com>
2020-06-08 14:51:56 -0500

    App is completely functional.

--- app.js
+++ app.js
@@ -1,16 +1,65 @@
-function round(value, decimals) {
-    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
-}
-/*used to get input for price of meal */
-var mealCost = parseFloat(prompt("What was the cost of your meal?"));
-var tipRate = 0.2;
-/*Calculate tip*/
-var tip = (mealCost * tipRate);
-/*Round tip to 2nd decimal place. */
-tip = round(tip, 2);
-/*Caclulate the cost of the meal plus the tip. */
-var totalCost = (mealCost + tip);
-/* Change totalCost to a string so it will always include the zero in the hundredths place (12.60 instead of 12.6) */
-totalCost = totalCost.toFixed(2);
+function calcTip() {
+  /*used to get inputs for price, percentage and amount of people */
+  var mealCost = document.getElementById("costInput").value;
+  var tipRate = document.getElementById("selectRate").value;
+  var peopleCount = document.getElementById("peopleInput").value;
+  
+  /* Validate all inputs */
+  if (isNaN(mealCost)) {
+    alert("Please enter a numerical value.");
+    return;
+  }
+  if (mealCost === "" || tipRate == 0) {
+    alert("Please enter the values.");
+    return;
+  }
+  
+  /* Determine whether var peopleCount is one, or more people, and if it is properly inputted. */
+  if (isNaN(peopleCount) || peopleCount < 1) {
+    alert("Please enter a numerical value greater than 0");
+    return;
+  }
+  else if (peopleCount == 1) {
+    peopleCount = 1;
+    document.getElementById("each").style.display = "none";
+    document.getElementById("person").innerHTML= " person";
+  }
+  else {
+    document.getElementById("each").style.display = "inline";
+  }
+  
+  /*Calculation of the tip */
+  var tip = (mealCost * tipRate);
+  
+  /*Round tip to 2nd decimal place. */
+  var roundedTip = Number(Math.round(tip+'e2')+'e-2');
 
-document.write("Tip: $",tip, ", Total Cost: $", totalCost);
+  mealCost = parseFloat(mealCost);
+  roundedTip = parseFloat(roundedTip);
+  peopleCount = parseFloat(peopleCount);
+  
+  /*Caclulate the cost of the meal plus the tip.  */
+  var totalCost = ((mealCost + roundedTip)/peopleCount); 
+  /*Round total to 2nd decimal place. */
+  var roundedTotal = Number(Math.round(totalCost+'e2')+'e-2');
+  
+  /* Next line ensures that two decimal points will always show up. */
+  roundedTip = roundedTip.toFixed(2);
+  roundedTotal = roundedTotal.toFixed(2);
+  
+
+  document.getElementsByClassName("tipDisplay")[0].style.display="block";
+  document.getElementsByClassName("totalDisplay")[0].style.display="block";
+  document.getElementById("finalTip").innerHTML = roundedTip;
+  document.getElementById("finalTotal").innerHTML = roundedTotal;
+
+}  
+
+/* Hide the output before the action is completed. */
+document.getElementsByClassName("tipDisplay")[0].style.display="none";
+document.getElementsByClassName("totalDisplay")[0].style.display="none";
+
+  /* Call the function with the Button */
+document.getElementById("calc-btn").onclick = function(){
+  calcTip();
+}
