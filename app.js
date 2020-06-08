0229a99c7ff36913358e026247c8247334535389
Jackson Foster <jacksonf1999@gmail.com>
2020-06-08 17:36:20 -0500

    More Responsive CSS. Also added another small function in the JS.

--- app.js
+++ app.js
@@ -21,11 +21,10 @@ function calcTip() {
   }
   else if (peopleCount == 1) {
     peopleCount = 1;
-    document.getElementById("each").style.display = "none";
-    document.getElementById("person").innerHTML= " person";
+    document.getElementsByClassName("each")[0].style.display = "none";
   }
   else {
-    document.getElementById("each").style.display = "inline";
+    document.getElementsByClassName("each")[0].style.display = "inline";
   }
   
   /*Calculation of the tip */
@@ -38,26 +37,41 @@ function calcTip() {
   roundedTip = parseFloat(roundedTip);
   peopleCount = parseFloat(peopleCount);
   
+  var dividedTip = (roundedTip/peopleCount);
   /*Caclulate the cost of the meal plus the tip.  */
   var totalCost = ((mealCost + roundedTip)/peopleCount); 
   /*Round total to 2nd decimal place. */
   var roundedTotal = Number(Math.round(totalCost+'e2')+'e-2');
+
   
   /* Next line ensures that two decimal points will always show up. */
   roundedTip = roundedTip.toFixed(2);
+  dividedTip = dividedTip.toFixed(2);
   roundedTotal = roundedTotal.toFixed(2);
   
 
   document.getElementsByClassName("tipDisplay")[0].style.display="block";
+  if (peopleCount == 1) {
+    peopleCount = 1;
+    document.getElementsByClassName("dividedDisplay")[0].style.display="none";
+    document.getElementsByClassName("each")[1].style.display="none";
+  }
+  else {
+    document.getElementsByClassName("dividedDisplay")[0].style.display="block";
+  }
   document.getElementsByClassName("totalDisplay")[0].style.display="block";
+
   document.getElementById("finalTip").innerHTML = roundedTip;
+  document.getElementById("dividedTip").innerHTML = dividedTip;
   document.getElementById("finalTotal").innerHTML = roundedTotal;
+  
 
 }  
 
 /* Hide the output before the action is completed. */
 document.getElementsByClassName("tipDisplay")[0].style.display="none";
 document.getElementsByClassName("totalDisplay")[0].style.display="none";
+document.getElementsByClassName("dividedDisplay")[0].style.display="none";
 
   /* Call the function with the Button */
 document.getElementById("calc-btn").onclick = function(){
