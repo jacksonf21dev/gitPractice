460c286b1ea2fd4acdb269764c012ba20ba51af6
Jackson Foster <jacksonf1999@gmail.com>
2020-06-07 19:41:42 -0500

    Reworked JS app to have smoother functionality and less code.

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-function tipCalc (a,b) {
-    return a * b;
+function round(value, decimals) {
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
-function totalCalc (a, b) {
-    return a + b;
-}
-
+/*used to get input for price of meal */
 var mealCost = parseFloat(prompt("What was the cost of your meal?"));
 var tipRate = 0.2;
-var tip = tipCalc(mealCost, tipRate);
-var totalCost = totalCalc(tip, mealCost);
-var roundedTip = tip.toFixed(2);
-var roundedCost = totalCost.toFixed(2);
+/*Calculate tip*/
+var tip = (mealCost * tipRate);
+/*Round tip to 2nd decimal place. */
+tip = round(tip, 2);
+/*Caclulate the cost of the meal plus the tip. */
+var totalCost = (mealCost + tip);
+/* Change totalCost to a string so it will always include the zero in the hundredths place (12.60 instead of 12.6) */
+totalCost = totalCost.toFixed(2);
 
-
-
-document.write("Tip: $",roundedTip, ", Total Cost: $", roundedCost);
+document.write("Tip: $",tip, ", Total Cost: $", totalCost);
